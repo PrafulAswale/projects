47fd5101ac65262aee36dda85eae0f186df6bdb4
Praful Aswale <prafulcode15@gmail.com>
2023-12-31 12:42:57 +0530

    Chunk Norris jokes

---
+++ 13_jokes/script.js
@@ -0,0 +1,22 @@
+const displayJokes = document.getElementById("display-joke");
+const newJoke = document.getElementById("getJoke");
+
+const requestUrl = "https://api.chucknorris.io/jokes/random";
+
+function getNewJoke() {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", requestUrl);
+  xhr.onreadystatechange = function () {
+    //   console.log(xhr.readyState);
+    if (xhr.readyState === 4) {
+      const data = JSON.parse(this.responseText);
+      console.log(data.value);
+      displayJokes.innerText = data.value;
+    }
+  };
+  xhr.send();
+}
+
+newJoke.addEventListener("click", () => {
+  getNewJoke();
+});
